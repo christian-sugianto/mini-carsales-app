d84a250a94abbcde5f98b09278f3e927af96a6b0
Christian Sugianto <christianhadinata.sugi@gmail.com>
2020-06-17 21:55:01 +1000

    update Car and Vehicle model

--- server/models/Car/Car.cs
+++ server/models/Car/Car.cs
@@ -1,7 +1,12 @@
+using System;
+
 public class Car : Vehicle
 {
-    public CarEngine CarEngine { get; set; }
-    public CarBodyType CarBodyType { get; set; }
+    public Guid CarEngineId { get; set; }
+    public Guid CarBodyTypeId { get; set; }
     public int Doors { get; set; }
     public int Wheels { get; set; }
+
+    public CarEngine CarEngine { get; set; }
+    public CarBodyType CarBodyType { get; set; }
 }
--- server/models/Vehicle/Vehicle.cs
+++ server/models/Vehicle/Vehicle.cs
@@ -3,6 +3,10 @@ using System;
 public class Vehicle
 {
     public Guid Id { get; set; }
+    public Guid VehicleTypeId { get; set; }
+    public Guid VehicleMakeId { get; set; }
+    public Guid VehicleModelId { get; set; }
+
     public VehicleType VehicleType { get; set; }
     public VehicleMake VehicleMake { get; set; }
     public VehicleModel VehicleModel { get; set; }
