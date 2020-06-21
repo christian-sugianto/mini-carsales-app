fe1b1ad38b8dc9c6e0f525312a374898757bbd2f
Christian Sugianto <christianhadinata.sugi@gmail.com>
2020-06-21 22:37:14 +1000

    add pages and components in the frontend, implement the home page ui

---
+++ frontend/src/assets/consts.ts
@@ -0,0 +1,7 @@
+const colors = {
+  primaryColor: '#2E3047',
+  secondaryColor: '#3BBA9C',
+  tertiaryColor: '#43455C',
+};
+
+export default colors;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../assets/consts';
+import { Select, MenuItem, withStyles, InputBase } from '@material-ui/core';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <HomeTitle> Mini Carsales Application</HomeTitle>
+      <div style={{ marginTop: '5rem' }}>
+        <CreateLabel>Create Vehicle: </CreateLabel>
+        <StyledSelect input={<BootstrapInput />}>
+          <MenuItem>Create Car</MenuItem>
+        </StyledSelect>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.primaryColor};
+  text-align: center;
+`;
+
+const HomeTitle = styled.h1`
+  color: ${colors.secondaryColor};
+`;
+
+const StyledSelect = styled(Select)`
+  display: inline-block;
+`;
+
+const CreateLabel = styled.h2`
+  display: inline-block;
+  color: ${colors.secondaryColor};
+  margin-right: 2rem;
+`;
+
+const BootstrapInput = withStyles((theme: any) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: colors.tertiaryColor,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      backgroundColor: colors.tertiaryColor,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
+
+export default Home;
